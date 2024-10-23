9f7621c355ff9974ed59faa4856b04f4762de1b0
Piero López <plopezrosas@danielcastelao.org~>
2024-10-23 11:02:18 +0200

    numeros primos en proceso

---
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/numerosPrimos.java
@@ -0,0 +1,35 @@
+package com.mycompany.Ejercicios;
+import java.util.Scanner;
+/**
+ * Solicito n -->   numeros primos entre 1 y el n
+ * @version 1.0
+ * @author Piero López Rosas
+ */
+public class numerosPrimos {
+    // Un numero primo es aquel que tiene dos divisores: el 1 y el mismo.
+    public static void main(String[] args){
+        Scanner op;
+        op = new Scanner(System.in);
+        System.out.println("Dame un  número y te diré sus");
+        int m = op.nextInt();
+
+
+        for ( int i = 1; i < m;){
+            if(i/1==i){
+                if(i/i==1);
+            }
+            i++;
+            System.out.println(i);
+        }
+    }
+}
+/*
+x/x  -->  int m
+          for ( int i = 1; i<m){
+            if(i/1==i){
+             if(i/i=0);
+            }
+          }
+
+y el x/1
+ */
