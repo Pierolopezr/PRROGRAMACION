3bf5f057545e2a3dacf6692e555a1f1dacd51653
Piero López <plopezrosas@danielcastelao.org~>
2024-10-23 09:28:12 +0200

    Multiplos de 7

---
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/Multiplos.java
@@ -0,0 +1,15 @@
+package com.mycompany.Ejercicios;
+/**
+ * Dame los múltiplos de 7 menores que 100
+ * @version 1.0
+ * @author Piero López Rosas
+ */
+public class Multiplos {
+    public static void main(String[] args){
+
+        for (int i = 0; i < 92;) {
+            i+=7;
+            System.out.println( i );
+        }
+    }
+}
