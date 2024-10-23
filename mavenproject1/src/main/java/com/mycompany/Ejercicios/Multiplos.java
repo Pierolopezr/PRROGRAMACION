0ff13ad03e41f7e7334cce6bb1bba6a5443085ed
Piero López <plopezrosas@danielcastelao.org~>
2024-10-23 09:40:11 +0200

    Tres maneras de dar múltiplos de 7 menores que 100.

--- mavenproject1/src/main/java/com/mycompany/Ejercicios/Multiplos.java
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/Multiplos.java
@@ -11,5 +11,18 @@ public class Multiplos {
             i+=7;
             System.out.println( i );
         }
+
+        System.out.println("\n");
+
+        for (int n=1; 7*n<100; n++){
+            System.out.println(7*n);
+        }
+
+        System.out.println("\n");
+
+        for (int n=0; n<100; n++){
+            if (n%7==0) System.out.println(n); // solo se puede hacer una linea, no es necesario poner llaves o punto y coma,
+                                               // inclusive con el else pero con un codigo de qué hacer, y al ; al final.
+        }
     }
 }
