2d84427530dfdd078af95049281cc1e68b59b671
piero <plopezrosas@danielcastelao.org>
2024-10-25 01:27:52 +0200

    Suma y producto de numeros (10-90). MODIFICADO

--- mavenproject1/src/main/java/com/mycompany/Ejercicios/Boletin5_2.java
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/Boletin5_2.java
@@ -8,7 +8,7 @@ public class Boletin5_2 {
     public static void main(String[] args){
 
         double a, sum= 10, producto= 10;  // Usamos double, pq el resultado del producto supera el limite de un long y un int.
-        System.out.println("Dame numeros entre 10 y 90: ");
+        System.out.println("Dame números entre 10 y 90: ");
         for(a=10; (a>=10 && a<=90); a++){  // añadimo un for pq sabemos la cantidad de numeros a desarrollar
 
            sum= sum + a;  // colocamos las formulas de suma y producto, respectivamente
