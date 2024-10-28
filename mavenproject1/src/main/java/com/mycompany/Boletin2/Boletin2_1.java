7dc84c6b10e409a44b132399ed318b81ce52cfc2
Piero López <plopezrosas@danielcastelao.org~>
2024-10-28 12:37:19 +0100

    base altura

---
+++ mavenproject1/src/main/java/com/mycompany/Boletin2/Boletin2_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.Boletin2;
+
+/**
+ *
+ * @author dam
+ */
+public interface Boletin2_1 {
+    public static void main (String [] args){
+       //Boletín 2 ejercicio 1
+       /*
+        Es muy importante dejar anotaciones en el código para hacer las aclaraciones que precisemos
+       */
+       //double base = 4,  altura = 3, area;
+        double base, altura, area;
+        base = 4;
+        altura = 3;
+        area= base * altura / 2;
+        System.out.println(area);
+    }
+}
