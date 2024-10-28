22af0387cec16e775b13ff4c98b17fb71d8d6680
Piero López <plopezrosas@danielcastelao.org~>
2024-10-28 12:41:24 +0100

    area lado

---
+++ mavenproject1/src/main/java/com/mycompany/Boletin2/Boletin2_2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Boletin2;
+
+/**
+ *area lado
+ * @author Piero López Rosas
+ */
+public class Boletin2_2 {
+    public static void main (String [] args){
+        double lado, area;
+        lado = 3;
+        area = lado * lado;
+        System.out.println(area);
+
+    }
+}    
