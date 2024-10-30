78e57d1f45b2eba3c199093060b7a6ffa2b98c56
Piero López <plopezrosas@danielcastelao.org~>
2024-10-30 10:02:23 +0100

    fUNCIONES I

---
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/EjemploFunciones.java
@@ -0,0 +1,21 @@
+package com.mycompany.Ejercicios;
+/**
+ * Explicación con funciones
+ * @version 1.0
+ * @author Piero López Rosas
+ */
+public class EjemploFunciones {
+    public static void main(String[] arga){
+        for (int i=0; i<3; i++){
+            System.out.println("Hola");
+        }
+        System.out.println("Otras tareas en el medio");
+        for (int i=0; i<3; i++) {
+            System.out.println("Hola");
+        }
+        System.out.println("Otras tareas en el medio");
+        for (int i=0; i<3; i++) {
+            System.out.println("Hola");
+        }
+    }
+}
