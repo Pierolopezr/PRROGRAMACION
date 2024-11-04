63f8adb476a097e9e2acc56f83241907bbaae537
Piero López <plopezrosas@danielcastelao.org~>
2024-11-04 13:28:47 +0100

    Beltin 6-1 añadiendo c y d redondeo de valores

--- mavenproject1/src/main/java/com/mycompany/Boletin6/Boletin6_1.java
+++ mavenproject1/src/main/java/com/mycompany/Boletin6/Boletin6_1.java
@@ -12,16 +12,30 @@ public class Boletin6_1 {
         System.out.println("Dame dos números enteros y te diré los que están compredidos entre ellos: ");
         int a = op.nextInt();
         int b = op.nextInt();
+        double c = 35.27;
+        double d = 29.56;
         numerosComprendidos(a, b);
+        numerosComprendidos(c, d);
+        numerosComprendidos(b);
     }
     static void numerosComprendidos(int num1, int num2){
        if( num1>num2){  // poner cual es mayor y cual es menor
            int aux = num1;
            num1 = num2;
-           num2 = aux;
+           num2 = aux; // aux : variable auxiliar que se necesita para cambiar el orden, num1 por num2 o viceversa
        }
         for (int i= num1 + 1; i < num2 ; i++){ // +1 para que empience en el siguiente
             System.out.println(i);
         }
+    }
+
+    static void numerosComprendidos(double num1, double num2){
+        int num1Int = (int) Math.round(num1);
+        int num2Int = (int) Math.round(num2);       // redondea los numeros
+        numerosComprendidos(num1Int, num2Int);
+    }
+
+    static void numerosComprendidos(int num1){
+        numerosComprendidos(0, num1);
     }
 }
