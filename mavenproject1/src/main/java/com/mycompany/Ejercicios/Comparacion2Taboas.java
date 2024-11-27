835abfeb8804fc89e89dce5ab9df70885fc12109
Piero López <plopezrosas@danielcastelao.org~>
2024-11-27 11:02:47 +0100

    Longitud de fila y altura de una tabla

---
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/Comparacion2Taboas.java
@@ -0,0 +1,10 @@
+package com.mycompany.Ejercicios;
+
+public class Comparacion2Taboas {
+    public static void main(String[] args){
+        int[] [] t1 = {{1, 2, 3},
+                      {4, 5, 6} };
+        System.out.println(t1[1].length); //Longitud de la fila
+        System.out.println(t1.length); //longitudad de la altura
+    }
+}
