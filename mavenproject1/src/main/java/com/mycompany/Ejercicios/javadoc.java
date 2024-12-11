224d10c810d3ef75fc72ff6c15a0b5284048a408
Piero López <plopezrosas@danielcastelao.org~>
2024-12-11 14:33:05 +0100

    javadoc

---
+++ mavenproject1/src/main/java/com/mycompany/Ejercicios/javadoc.java
@@ -0,0 +1,44 @@
+package com.mycompany.Ejercicios;
+
+/**
+ * jUEGO DE HUNDIR LA FLOTA
+ * @version 1.0
+ * @author Piero López // El orden no importa
+ */
+public class javadoc {
+    /**
+     *Funcióm principal
+     * @param args  parametros del juego // el args lo copia del argumento de abajo del public "args"
+     */
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * recorre el tablero de juego
+     * @param tablero array de dos dimensiones , el tablero de los barcos
+     */
+    private static void recorrerTablero(int[][] tablero){
+
+    }
+
+    /**
+     * Comprueba si el valor es una nave o no
+     * @param valor_coordenada valor de la coordenada a estudiar
+     * @return true en caso de ser uma nave
+     */
+    static boolean esNave(int valor_coordenada){
+        return valor_coordenada>0;
+    }
+
+    /**
+     * Muestra por pantalla el tipo de nave segun valor coordenada
+     * También miestra la coordenada y la ordenada
+     * @param valor_coordenada valor de la coordenada
+     * @param x coordenada, eje horizontal
+     * @param y ordenada, eje vertical
+     */
+    static void tipoDeNave(int valor_coordenada, int x, int y){
+
+    }
+}
